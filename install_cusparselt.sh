8f7816f14dc846be67d423424b402413b1d1f993
helloimsuhyun <chsuk02@hanyang.ac.kr>
2026-04-08 18:05:06 +0900

    try

--- install_cusparselt.sh
+++ install_cusparselt.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-
-CUDA_VERSION = 12.6
 set -ex
 
-# cuSPARSELt license: https://docs.nvidia.com/cuda/cusparselt/license.html
-mkdir tmp_cusparselt && cd tmp_cusparselt
+CUDA_VERSION=12.6
 
-if [[ ${CUDA_VERSION:0:4} =~ ^12\.[1-4]$ ]]; then
+mkdir -p tmp_cusparselt
+cd tmp_cusparselt
+
+if [[ ${CUDA_VERSION:0:4} =~ ^12\.[1-6]$ ]]; then
     arch_path='sbsa'
     export TARGETARCH=${TARGETARCH:-$(uname -m)}
-    if [ ${TARGETARCH} = 'amd64' ] || [ "${TARGETARCH}" = 'x86_64' ]; then
+    if [ "${TARGETARCH}" = 'amd64' ] || [ "${TARGETARCH}" = 'x86_64' ]; then
         arch_path='x86_64'
     fi
     CUSPARSELT_NAME="libcusparse_lt-linux-${arch_path}-0.5.2.1-archive"
-    curl --retry 3 -OLs https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-${arch_path}/${CUSPARSELT_NAME}.tar.xz
+    curl --retry 3 -OLs "https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-${arch_path}/${CUSPARSELT_NAME}.tar.xz"
 elif [[ ${CUDA_VERSION:0:4} == "11.8" ]]; then
     CUSPARSELT_NAME="libcusparse_lt-linux-x86_64-0.4.0.7-archive"
-    curl --retry 3 -OLs https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-x86_64/${CUSPARSELT_NAME}.tar.xz
+    curl --retry 3 -OLs "https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse_lt/linux-x86_64/${CUSPARSELT_NAME}.tar.xz"
+else
+    echo "Unsupported CUDA_VERSION: ${CUDA_VERSION}"
+    exit 1
 fi
 
-tar xf ${CUSPARSELT_NAME}.tar.xz
-cp -a ${CUSPARSELT_NAME}/include/* /usr/local/cuda/include/
-cp -a ${CUSPARSELT_NAME}/lib/* /usr/local/cuda/lib64/
+ls -lh
+tar xf "${CUSPARSELT_NAME}.tar.xz"
+sudo cp -a "${CUSPARSELT_NAME}"/include/* /usr/local/cuda/include/
+sudo cp -a "${CUSPARSELT_NAME}"/lib/* /usr/local/cuda/lib64/
 cd ..
 rm -rf tmp_cusparselt
-ldconfig
+sudo ldconfig
