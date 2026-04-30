7911fe31384f016eb5b322676573ecdca49e6507
helloimsuhyun <chsuk02@hanyang.ac.kr>
2026-04-30 20:51:56 +0900

    리얼센스 파라미터 문제 해결

--- src/capston_bringup/launch/realsense_wrapper.launch.py
+++ src/capston_bringup/launch/realsense_wrapper.launch.py
@@ -1,24 +1,21 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
-
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    realsense_dir = get_package_share_directory('realsense2_camera')
-
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(realsense_dir, 'launch', 'rs_launch.py')
-            ),
-            launch_arguments={
+        Node(
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            namespace='camera',
+            name='camera',
+            output='screen',
+            parameters=[{
                 'rgb_camera.color_profile': '1280x720x30',
                 'depth_module.depth_profile': '1280x720x30',
-                'enable_depth': 'true',
-                'align_depth.enable': 'true',
-                'rgb_camera.enable_auto_exposure': 'true',
-            }.items()
+                'enable_depth': True,
+                'enable_color': True,
+                'align_depth.enable': True,
+                'rgb_camera.enable_auto_exposure': True,
+            }]
         )
     ])
